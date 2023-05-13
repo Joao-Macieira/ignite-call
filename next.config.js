c1d70a4550b3f47d68b29aeffaa2678f76efe9b7
João Macieira <jp.macieira@uol.com.br>
2023-05-13 17:06:31 -0300

    fix: adjust next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  experimental: {
+    appDir: false,
+  },
 };
 
 module.exports = nextConfig;
